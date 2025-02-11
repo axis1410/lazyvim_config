6565494de6cc0142ccac61f7219b62b4600520bc
Aditya Singh <adityasingh14102001@gmail.com>
2025-02-11 17:37:42 +0530

    Initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+  },
+  {
+    "decaycs/decay.nvim",
+    lazy = false,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "decay-default",
+      indentguides = false,
+    },
+  },
+}
