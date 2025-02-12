a4356e5d800b4cff5295d95db3ad66f7f0d141a2
Aditya Singh <adityasingh14102001@gmail.com>
2025-02-12 18:34:07 +0530

    add keymaps and new themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,6 +7,19 @@ return {
   {
     "navarasu/onedark.nvim",
     lazy = false,
+    opts = {
+      code_style = {
+        comments = "italic",
+        keywords = "none",
+        functions = "none",
+        strings = "none",
+        variables = "none",
+      },
+      style = "dark", -- Choose from "dark", "darker", "cool", "deep", "warm", "warmer", (do not choose) "light"
+      lualine = {
+        transparent = true,
+      },
+    },
   },
   {
     "decaycs/decay.nvim",
@@ -15,7 +28,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "decay-default",
+      colorscheme = "onedark",
       indentguides = false,
     },
   },
